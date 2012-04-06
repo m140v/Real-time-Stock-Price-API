223b5168c4107d1a685a60efcef1ef0384fec99b
Mike De'Shazer <snipsnipsnip1@gmail.com>
2012-04-05 21:16:45 -0400

    this is the first commit

---
+++ index.php
@@ -0,0 +1,112 @@
+<?php
+
+
+// get ticker symbol and assign to variable $ticker
+extract($_REQUEST);
+
+if(isset($ticker)){
+//check if $ticker is set
+$ticker = strtoupper($ticker);
+
+//URLs (valid on 4/8/2012) 
+//These need to be changed as the formats of the pages change over time
+$reuters= "http://reuters.com/finance/stocks/overview?symbol=". $ticker;
+$nasdaq= "http://www.nasdaq.com/symbol/". $ticker;
+$yahoo= "http://finance.yahoo.com/q?s=". $ticker;
+
+
+//Try to retrieve Reuters price first (most reliable of 3 possible sources)
+$reutResult = file_get_contents($reuters);
+$nyArr1 = explode( 'font-size: 23px;">', $reutResult);
+if($nyArr1[1]){
+$nyArr2 = explode( "</span>", $nyArr1[1]);
+if($nyArr2[1]){
+$nyPrice = $nyArr2[0];
+}
+}
+
+
+if($nyPrice){
+    // We have Reuter's price data for this stock
+     $jsonResponse = '{"price": "'.floatval($nyPrice).'", "source": "Reuters"}';
+     echo json_encode($jsonResponse);
+    return;
+
+}
+
+
+
+else{
+
+//could not get Reuters, so trying Nasdaq
+ $nasResult = file_get_contents($nasdaq);   
+ //Try to retrieve Nasdaq price:
+$nasArr1 = explode( "_LastSale1'>", $nasResult);
+if($nasArr1[1]){
+$nasArr2 = explode( "</label>", $nasArr1[1]);
+if($nasArr2[1]){
+$nasPrice = $nasArr2[0];
+}
+}
+
+
+if($nasPrice){
+    //we have Nasdaq's price
+    $nasPrice = str_replace("$", "", $nasPrice);
+    $nasPrice = str_replace(" ", "", $nasPrice);
+     $jsonResponse = '{"price": "'. $nasPrice.'", "source": "Nasdaq"}';
+     echo json_encode($jsonResponse);
+    //return;
+
+}
+
+
+
+else{
+    //could not get Nasdaq or Reutors, so trying Yahoo
+    $yahResult = file_get_contents($yahoo);
+
+$ticker = strtolower($ticker);
+$yahArr1 = explode( 'id="yfs_l84_'.$ticker.'">', $yahResult);
+if($yahArr1[1]){
+   // echo $yahArr1[1];
+$yahArr2 = explode( " ", $yahArr1[1]);
+if($yahArr2[1]){
+   
+$yahPrice = $yahArr2[0];
+}
+}
+
+
+if($yahPrice){
+     $jsonResponse = '{"price": "'.floatval($yahPrice).'" , "source": "Yahoo"}';
+     echo json_encode($jsonResponse);
+    //return;
+
+}
+
+else{
+      $jsonResponse = '{"error": "You either entered an invalid ticker or this script cannot scrape the source sites successfully Pass in AAPL as ticker. If you get this error still, Please check Github for the latest version."}';
+     echo json_encode($jsonResponse);
+    return;
+
+}
+
+}
+
+}
+
+}
+
+
+else{
+    $jsonResponse = '{"error": "please send a ticker symbol in your request with the key `ticker`."}';
+     echo json_encode($jsonResponse);
+    return;
+    }
+
+
+
+
+
+?>
