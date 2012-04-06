84644ef6d38be889f9587f00d06ee3f3241cebba
Mike De'Shazer <snipsnipsnip1@gmail.com>
2012-04-06 11:55:54 -0400

    Working in near-realtime, but of course could be improved with realer-time data sources. The more scrape fallback the merrier.

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
 
 
-// get ticker symbol and assign to variable $ticker
+/* This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, Version 3.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    */
+
+//author: Mike De'Shazer || @itsdeshazer
+
+
+// Start by getting the ticker symbol and assigning it to variable $ticker
 extract($_REQUEST);
 
 if(isset($ticker)){
